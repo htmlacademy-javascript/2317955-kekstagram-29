b57f260aa3f8e28895b1654a78ad9e78bd5e233f
urchifox <126313096+urchifox@users.noreply.github.com>
2023-06-21 13:34:09 +0400

    5.14. Модуляция delete console.log

--- js/main.js
+++ js/main.js
@@ -3,7 +3,5 @@ import {makeFotos} from './make-fotos.js';
 import {FOTOS_AMOUNT} from './data.js';
 
 getRandomFotos(FOTOS_AMOUNT);
-console.log('getRandomFotos', getRandomFotos(FOTOS_AMOUNT));
 
 makeFotos(FOTOS_AMOUNT);
-console.log('makeFotos', makeFotos(FOTOS_AMOUNT));
