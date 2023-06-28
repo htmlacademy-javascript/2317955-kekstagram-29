584b118f29882a093b8a25bbf2c6ac057641661c
urchifox <126313096+urchifox@users.noreply.github.com>
2023-06-28 14:30:47 +0400

    7.15. Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-import {getRandomFotosV1} from './get-random-fotos-v1.js';
-import {getRandomFotosV2} from './get-random-fotos-v2.js';
-import {FOTOS_AMOUNT} from './constants.js';
+import './render-pictures.js';
 import './upload-foto.js';
 import './edit-foto.js';
 import './validation.js';
@@ -8,8 +6,3 @@ import './errors.js';
 import './see-picture.js';
 import './comments.js';
 import './filter-fotos.js';
-
-
-getRandomFotosV1(FOTOS_AMOUNT);
-
-getRandomFotosV2(FOTOS_AMOUNT);
