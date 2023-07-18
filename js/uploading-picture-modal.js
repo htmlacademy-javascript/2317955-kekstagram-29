74c721c263397af0dec99359e36ac7d928ca155c
urchifox <126313096+urchifox@users.noreply.github.com>
2023-07-18 13:55:41 +0400

    12.13. Фото на память make preview from uploaded picture

--- js/uploading-picture-modal.js
+++ js/uploading-picture-modal.js
@@ -2,6 +2,7 @@ import {resetValidator} from './form-validation.js';
 import {isTextFieldActive} from './util.js';
 import {setFormSubmit} from './uploading-picture-form.js';
 
+const ACCEPTABLE_FILE_TYPES = ['jpg', 'jprg', 'png'];
 
 const form = document.querySelector('.img-upload__form');
 const uploadModal = form.querySelector('.img-upload__overlay');
@@ -9,7 +10,8 @@ const imgInput = form.querySelector('.img-upload__input');
 const hashtagInput = form.querySelector('.text__hashtags');
 const commentInput = form.querySelector('.text__description');
 const formCloseBtn = form.querySelector('.img-upload__cancel');
-const preview = form.querySelector('.img-upload__preview');
+const preview = form.querySelector('.img-upload__preview img');
+const effectsPreviews = form.querySelectorAll('.effects__preview');
 const scaleInput = form.querySelector('.scale__control--value');
 
 
@@ -52,6 +54,16 @@ function onDocumentEscape (evt) {
 }
 
 const onInputImgChange = () => {
+  const file = imgInput.files[0];
+  const fileName = file.name.toLowerCase();
+  const isNameAcceptable = ACCEPTABLE_FILE_TYPES.some((type) => fileName.endsWith(type));
+  if(isNameAcceptable) {
+    preview.src = URL.createObjectURL(file);
+    effectsPreviews.forEach((effectPreview) => {
+      effectPreview.style.backgroundImage = `url(${preview.src})`;
+    });
+  }
+
   openModal();
 };
 
